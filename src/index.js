4b394e0ce5e0a3b15745bc199e1aed9c55e88491
383myrtle <89387525+383myrtle@users.noreply.github.com>
2025-03-11 17:08:04 +0200

    Initialize and render boards upon page load

--- src/index.js
+++ src/index.js
@@ -1,2 +1,11 @@
 import "./styles.css";
 import "./normalize.css";
+
+import { renderPlayerBoard, renderOpponentBoard } from "./DisplayController.js";
+import { Player } from "./Player.js";
+
+const player = new Player();
+const opponent = new Player();
+player.initializeRandomBoard();
+renderPlayerBoard(player.gameboard);
+renderOpponentBoard(opponent.gameboard);
