6a27d2c011250763c7c50ec9d9875ddac85daa03
383myrtle <89387525+383myrtle@users.noreply.github.com>
2025-03-12 10:52:05 +0200

    refactor: move cell creation logic and event listeners
    Cell creation now handled by the createCell function. Cell click event listeners now moved to Game module.

--- src/DisplayController.js
+++ src/DisplayController.js
@@ -3,32 +3,30 @@ const renderBoard = (player) => {
   grid.textContent = "";
   for (let y = 9; y >= 0; y--) {
     for (let x = 0; x < 10; x++) {
-      const cell = document.createElement("div");
-      cell.classList.add("cell");
-      cell.dataset.x = x;
-      cell.dataset.y = y;
-      if (player.gameboard.board[x][y] && player.type === "player") {
-        cell.classList.add("has-ship");
-      }
-      if (player.gameboard.receivedAttacks[x][y] === 1) {
-        if (player.gameboard.board[x][y]) {
-          cell.classList.add("hit");
-        } else {
-          cell.classList.add("miss");
-        }
-        cell.textContent = "X";
-      }
-      if (player.type === "opponent") {
-        cell.addEventListener("click", () => {
-          const x = parseInt(cell.dataset.x);
-          const y = parseInt(cell.dataset.y);
-          player.gameboard.receiveAttack(x, y);
-          renderBoard(player);
-        });
-      }
+      const cell = createCell(player, x, y);
       grid.appendChild(cell);
     }
   }
 };
+
+function createCell(player, x, y) {
+  const cell = document.createElement("div");
+  cell.classList.add("cell");
+  cell.dataset.x = x;
+  cell.dataset.y = y;
+
+  if (player.gameboard.board[x][y] && player.type === "player") {
+    cell.classList.add("has-ship");
+  }
+  if (player.gameboard.receivedAttacks[x][y] === 1) {
+    if (player.gameboard.board[x][y]) {
+      cell.classList.add("hit");
+    } else {
+      cell.classList.add("miss");
+    }
+    cell.textContent = "X";
+  }
+  return cell;
+}
 
 export { renderBoard };
