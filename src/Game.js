37812acb89d543669163678874fac2648dca249c
383myrtle <89387525+383myrtle@users.noreply.github.com>
2025-03-13 09:28:51 +0200

    feat: add basic game loop

--- src/Game.js
+++ src/Game.js
@@ -1,6 +1,6 @@
 import { Player } from "./Player.js";
 import { renderBoard } from "./DisplayController.js";
-import { handleAttacks } from "./EventHandlers.js";
+import { handleAttack } from "./EventHandlers.js";
 
 export class Game {
   constructor(playerName) {
@@ -16,10 +16,51 @@ export class Game {
     renderBoard(this.opponent);
   }
 
-  start() {
+  async start() {
     const opponentGrid = document.querySelector(".opponent-board .board");
-    opponentGrid.addEventListener("click", (e) => {
-      handleAttacks(e, this.opponent);
+
+    let result = this.checkWin();
+    while (result === null) {
+      await this.playerAttack(opponentGrid, this.opponent);
+      result = this.checkWin();
+      if (result !== null) {
+        break;
+      }
+      this.randomOpponentAttack();
+      result = this.checkWin();
+    }
+  }
+
+  playerAttack(opponentGrid, opponent) {
+    return new Promise((resolve) => {
+      opponentGrid.addEventListener("click", function handler(e) {
+        handleAttack(e, opponent);
+        opponentGrid.removeEventListener("click", handler);
+        resolve();
+      });
     });
+  }
+
+  randomOpponentAttack() {
+    let x = Math.round(9 * Math.random());
+    let y = Math.round(9 * Math.random());
+
+    while (this.player.gameboard.receivedAttacks[x][y]) {
+      x = Math.round(9 * Math.random());
+      y = Math.round(9 * Math.random());
+    }
+
+    this.player.gameboard.receiveAttack(x, y);
+    renderBoard(this.player);
+  }
+
+  checkWin() {
+    if (this.player.gameboard.allShipsSunk()) {
+      return this.player;
+    }
+    if (this.opponent.gameboard.allShipsSunk()) {
+      return this.opponent;
+    }
+    return null;
   }
 }
